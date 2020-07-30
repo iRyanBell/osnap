113658952d8d9234ec50618ae0bb9969c489e07f
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2020-07-30 11:19:29 -0700

    Status tab

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -1,6 +1,57 @@
-import React from "react";
-import { Container } from "@material-ui/core";
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Container } from "@material-ui/core";
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@material-ui/core";
+import { useIPFS } from "../hooks";
+import { IpfsGatewayContext, Web3Context } from "../context";
+import { ipfsGatewayBaseURL } from "../utils/ipfs";
+
+const useStyles = makeStyles({
+  tableHeader: {
+    fontWeight: "bold",
+  },
+});
 
 export default function Status() {
-  return <Container maxWidth="lg">Status</Container>;
+  const peerID = useIPFS();
+  const classes = useStyles();
+  const ethAddr = useContext(Web3Context);
+  const isLocalIPFSGateway = useContext(IpfsGatewayContext);
+
+  const gatewayBaseURL = ipfsGatewayBaseURL({ isLocalIPFSGateway });
+
+  const rows = [
+    { name: "IPFS: Peer ID", value: peerID || "..." },
+    { name: "IPFS: Gateway", value: gatewayBaseURL },
+    { name: "Ethereum: Wallet Address", value: ethAddr || "..." },
+  ];
+
+  return (
+    <Container maxWidth="lg">
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell
+                  className={classes.tableHeader}
+                  component="th"
+                  scope="row"
+                >
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
