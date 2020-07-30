079015a9ad6c4e2a16cde94a3d66436892017088
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2020-07-30 11:09:56 -0700

    IPFS Gateway Switch state propagation

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { Route, HashRouter } from "react-router-dom";
 import { TabbedRoutes } from "../components";
@@ -6,6 +6,18 @@ import { TabbedRoutes } from "../components";
 import theme from "./theme";
 
 export default function App() {
+  const [isLocalIPFSGateway, setIsLocalIPFSGateway] = useState(
+    Boolean(Number(window.localStorage.getItem("localIPFSGateway")))
+  );
+
+  const handleToggleIPFSGateway = () => {
+    window.localStorage.setItem(
+      "localIPFSGateway",
+      Number(!isLocalIPFSGateway)
+    );
+    setIsLocalIPFSGateway(!isLocalIPFSGateway);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -14,8 +26,8 @@ export default function App() {
           path="/"
           render={(props) => (
             <TabbedRoutes
-              isLocalIPFSGateway={false}
-              onToggleIPFSGateway={() => {}}
+              isLocalIPFSGateway={isLocalIPFSGateway}
+              onToggleIPFSGateway={handleToggleIPFSGateway}
               {...props}
             />
           )}
